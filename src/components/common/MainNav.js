49eb667643d14a27d2f6cc7fafd60cc44b0b706e
mjbaga <mjdbaga@gmail.com>
2022-03-04 12:36:31 +0800

    Finish up header mobile navigation

--- src/components/common/MainNav.js
+++ src/components/common/MainNav.js
@@ -1,29 +1,25 @@
 import { NavLink } from 'react-router-dom';
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import menuIcon from 'assets/images/shared/tablet/icon-hamburger.svg';
 
-const MainNav = (props) => {
-  const { navItems, id } = props;
-  const [menuIsActive, setMenuIsActive] = useState(false);
+const MainNav = ({ navItems, id, menuActive, onMenuBtnClick }) => {
 
   const menuClickHandler = (e) => {
-    setMenuIsActive(!menuIsActive);
+    onMenuBtnClick()
   }
-
-  menuIsActive ? document.body.classList.add('menu-open') : document.body.classList.remove('menu-open');
 
   return (
     <Fragment>
       <button 
         className="bg-transparent border-0 order-1 sm:grow-1 lg:hidden"
         aria-controls={id}
-        aria-expanded={menuIsActive ? 'true' : 'false'}
+        aria-expanded={menuActive ? 'true' : 'false'}
         onClick={menuClickHandler} 
       >
         <span className="sr-only">Menu Button</span>
         <img src={menuIcon} alt="Menu button" />
       </button>
-      <nav className={`main-nav absolute lg:relative order-2 left-0 top-[90px] lg:top-auto lg:left-auto hidden lg:block ${menuIsActive ? 'active' : '' }`}>
+      <nav className="main-nav hidden lg:block order-2">
         <ul id={id ? id : ''} className="flex">
           <li className="nav-item">
             <NavLink to="/" className="text-white hidden uppercase lg:inline text-xs text-center mx-4 hover:text-primary transition-colors duration-300 font-bold">
