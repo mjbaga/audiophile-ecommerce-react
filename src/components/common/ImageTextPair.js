a1a90a31b7cab93c9fe283c96028a390b1dd3815
mjbaga <mjdbaga@gmail.com>
2022-03-08 15:13:11 +0800

    Set image as first element default for mobile on ImageTextPair component

--- src/components/common/ImageTextPair.js
+++ src/components/common/ImageTextPair.js
@@ -3,7 +3,7 @@ import ImageSet from "./ImageSet";
 const ImageTextPair = ({ children, imageSet, order="image" }) => {
   return (
     <div className="image-text-pair flex flex-col lg:flex-row gap-8 lg:gap-4">
-      <ImageSet classes={`rounded-lg overflow-hidden lg:w-1/2 ${order === 'image' ? 'lg:order-1' : 'order-2'}`} imageSet={imageSet} />
+      <ImageSet classes={`rounded-lg overflow-hidden lg:w-1/2 order-1 ${order === 'image' ? 'lg:order-1' : 'order-2'}`} imageSet={imageSet} />
       <div className={`text-container text-center lg:text-left lg:w-1/2 flex flex-col justify-center ${order === 'image' ? 'lg:order-2 lg:px-16 xl:px-24' : 'order-1 lg:pr-8 xl:pr-24'}`}>
         {children}
       </div>
