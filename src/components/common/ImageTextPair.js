c1fd1c2100691be71912016a3225ed88363c13b4
mjbaga <mjdbaga@gmail.com>
2022-03-09 09:41:33 +0800

    Update ImageText Pair component

--- src/components/common/ImageTextPair.js
+++ src/components/common/ImageTextPair.js
@@ -1,10 +1,22 @@
-import ImageSet from "./ImageSet";
+import ImageSet from "components/common/ImageSet";
+import CloudImageSet from "components/common/CloudImageSet";
 
-const ImageTextPair = ({ children, imageSet, order="image" }) => {
+const ImageTextPair = ({ children, title = "image", imageSet, order="image", cloud = false, classes = "" }) => {
+
+  const imageClasses = `rounded-lg overflow-hidden lg:w-1/2 order-1 ${order === 'image' ? 'lg:order-1' : 'lg:order-2'}`;
+  const imageDisplay = cloud ? 
+                        <CloudImageSet 
+                          imageSet={imageSet} 
+                          title={title} 
+                          classes={imageClasses} /> :
+                        <ImageSet 
+                          classes={imageClasses} 
+                          imageSet={imageSet} />;
+
   return (
-    <div className="image-text-pair flex flex-col lg:flex-row gap-8 lg:gap-4">
-      <ImageSet classes={`rounded-lg overflow-hidden lg:w-1/2 order-1 ${order === 'image' ? 'lg:order-1' : 'order-2'}`} imageSet={imageSet} />
-      <div className={`text-container text-center lg:text-left lg:w-1/2 flex flex-col justify-center ${order === 'image' ? 'lg:order-2 lg:px-16 xl:px-24' : 'order-1 lg:pr-8 xl:pr-24'}`}>
+    <div className={`image-text-pair flex flex-col lg:flex-row gap-8 lg:gap-4 ${classes ? classes : ''}`}>
+      { imageDisplay }
+      <div className={`text-container text-center lg:text-left lg:w-1/2 flex flex-col justify-center order-2 ${order === 'image' ? 'lg:order-2 lg:px-16 xl:px-24' : 'lg:order-1 lg:pr-8 xl:pr-24'}`}>
         {children}
       </div>
     </div>
