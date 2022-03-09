b6c6be72d77a614ed107e7d5fbd1d9c795fee605
mjbaga <mjdbaga@gmail.com>
2022-03-09 09:42:18 +0800

    Add products on category page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,12 +1,8 @@
-// import HeroBanner from "components/home/HeroBanner";
-// import ImageNav from "components/common/ImageNav";
-// import { $navItems } from "utils/Constants";
 import Button from "components/common/Button";
 import ImageTextPair from "components/common/ImageTextPair";
 import { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCategoryBySlug, getProductsByCat } from 'utils/Firebase-Api';
-import Image from "components/common/Image";
 
 const Category = () => {
   const [category, setCategory] = useState({});
@@ -34,15 +30,24 @@ const Category = () => {
       <section className="bg-black py-10 lg:py-20">
         <div className="container mx-auto px-5 lg:px-0">
           <h1 className="text-white text-center text-xl md:text-4xl uppercase">{category.name}</h1>
+        </div>
+      </section>
+      
+      <section className="bg-white py-10 lg:py-20">
+        <div className="container mx-auto px-5 lg:px-0">
           {products.length && (
-            products.map((product) => (
-              <ImageTextPair key={product.id} imageSet={product.image}>
+            products.map((product, i) => (
+              <ImageTextPair classes="mb-24" key={product.id} cloud="true" title={product.name} imageSet={product.image} order={i % 2 ? "text" : "image"}>
                 {product.new && <p className="uppercase text-primary tracking-overline text-xs">New Product</p>}
+                <h2 className="uppercase leading-10 text-xl my-2 md:my-8 tracking-ap-2 font-bold md:text-4xl md:leading-11 md:max-w-md md:mx-auto lg:ml-0">{product.name}</h2>
+                <p className="text-black opacity-50 my-2">{product.description}</p>
+                <Button to={`/products/${product.slug}`} classes="my-4 text-white bg-primary mx-auto lg:ml-0 hover:bg-secondary">See product</Button>
               </ImageTextPair>
             ))
           )}
         </div>
       </section>
+
     </Fragment>
   )
 }
