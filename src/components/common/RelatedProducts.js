db351c281f261e24282b036560c88f8f69f97925
mjbaga <mjdbaga@gmail.com>
2022-03-09 22:18:47 +0800

    Update spacing on mobile

--- src/components/common/RelatedProducts.js
+++ src/components/common/RelatedProducts.js
@@ -11,7 +11,7 @@ const RelatedProducts = ({ relatedItems }) => {
       <h2 className="uppercase text-black text-lg md:text-2xl font-bold my-4 text-center">You may also like</h2>
       <div className="flex flex-col md:flex-row gap-4 lg:gap-8">
         {relatedItems.map((item) => (
-          <div className="flex flex-col text-center my-8" key={item.slug}>
+          <div className="flex flex-col text-center my-4" key={item.slug}>
             <CloudImageSet imageSet={item.image} title={item.name} classes="my-4 rounded-lg overflow-hidden" />
             <h3 className="text-lg uppercase text-black font-bold my-4">{item.name}</h3>
             <Button to={`/products/${item.slug}`} classes="my-4 text-white bg-primary mx-auto hover:bg-secondary">See product</Button>
