f6690a1a911e369c4d8152a323e6596e58f5f6e5
mjbaga <mjdbaga@gmail.com>
2022-03-12 10:35:14 +0800

    Minor adjustment for mobile menu

--- src/components/common/ImageNav.js
+++ src/components/common/ImageNav.js
@@ -14,8 +14,8 @@ const ImageNav = ({ classes, menuIsActive, items, mobileMenu=false, id }) => {
           <li className="nav-item grow" key={i}>
             <NavLink to={`/categories/${item.slug}`} className="uppercase text-xs text-center sm:mx-2 group font-bold flex flex-col justify-end before:block before:absolute before:bg-darkgray before:w-full before:h-3/4 sm:before:h-3/5 before:bottom-0 before:left-0 relative py-4 before:z-0 before:rounded-lg h-full">
               <div className="relative">
-                <img className="mx-auto w-2/6 -mb-6 sm:w-2/3 lg:-mb-4" src={item.image} alt={item.name} />
-                <p className="uppercase text-sm mb-3 font-bold md:text-normal text-black group-hover:text-primary lg:text-md transition-colors duration-300">{item.name}</p>
+                <img className="mx-auto w-2/6 -mb-4 sm:w-2/3 lg:-mb-4" src={item.image} alt={item.name} />
+                <p className="uppercase text-sm mb-1 lg:mb-4 font-bold md:text-normal text-black group-hover:text-primary lg:text-md transition-colors duration-300">{item.name}</p>
                 <p className="right-arrow uppercase text-black/50 group-hover:text-primary transition-colors duration-300">Shop</p>
               </div>
             </NavLink>
